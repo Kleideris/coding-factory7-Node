cd6e2db4ada026d53c65ac5c01c41816640240cb
Ch. Kleideris <kleiderisch@gmail.com>
2025-04-02 14:40:38 +0300

    feat: user.controller, product.model, user.model, user.routes

--- usersApp/app.js
+++ usersApp/app.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+const user = require('./routes/user.routes.js')
+
+app.use('/api/users', user)
+
 module.exports = app
